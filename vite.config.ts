921671633ad0db57129e1675f189f0e42da7230d
hamitcf1 <67213698+hamitcf1@users.noreply.github.com>
2026-04-18 23:51:37 +0300

    fixes 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-import { copyFileSync } from 'fs';
-import { fileURLToPath } from 'url';
 
 
 export default defineConfig(({ mode }) => {
@@ -16,20 +14,6 @@ export default defineConfig(({ mode }) => {
     base: '',
     plugins: [
       react(),
-      {
-        name: 'copy-redirects',
-        closeBundle: () => {
-          try {
-            const __filename = fileURLToPath(import.meta.url);
-            const __dirname = resolve(__filename, '..');
-            const source = resolve(__dirname, 'public/_redirects');
-            const dest = resolve(__dirname, 'dist/_redirects');
-            copyFileSync(source, dest);
-          } catch (e) {
-            console.error('Error copying _redirects file:', e);
-          }
-        },
-      },
     ],
     build: {
       outDir: 'dist',
